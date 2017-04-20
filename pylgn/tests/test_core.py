681d7261dfd5e36df8028fc14e984bd11ef439a8
Milad <miladh@ibv.uio.no>
2017-04-20 17:03:41 +0200

    clean up core tests

--- pylgn/tests/test_core.py
+++ pylgn/tests/test_core.py
@@ -49,7 +49,7 @@ def test_core():
 
     # create stimulus
     k_g = integrator.spatial_freqs[6]
-    w_g = -integrator.temporal_freqs[40]
+    w_g = -integrator.temporal_freqs[1]
     stimulus = pylgn.stimulus.create_patch_grating_ft(angular_freq=w_g,
                                                       wavenumber=k_g,
                                                       orient=0.0,
@@ -81,38 +81,3 @@ def test_core():
 
     # clear network
     network.clear()
-
-
-@pytest.mark.core
-def test_minimal():
-    # create network
-    network = pylgn.Network()
-
-    # create integrator
-    integrator = network.create_integrator(nt=5, nr=7, dt=1, dr=1)
-
-    # create neurons
-    ganglion = network.create_ganglion_cell()
-    relay = network.create_relay_cell()
-
-    # create kernels
-    Krg_r = spl.create_gauss_ft()
-    Krg_t = tpl.create_delta_ft()
-
-    # connect neurons
-    network.connect(ganglion, relay, (Krg_r, Krg_t))
-
-    # create stimulus
-    k_g = integrator.spatial_freqs[3]
-    w_g = -integrator.temporal_freqs[1]
-    stimulus = pylgn.stimulus.create_fullfield_grating_ft(angular_freq=w_g,
-                                                          wavenumber=k_g,
-                                                          orient=0.0)
-    network.set_stimulus(stimulus)
-
-    # compute
-    network.compute_irf(relay)
-    network.compute_response(relay)
-
-    # visulize
-    # pylgn.plot.animate_cube(relay.response, title="Relay cell response")

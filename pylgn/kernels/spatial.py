f181c9b06098522c2b34edc758c4913364efc8b7
Milad <miladh@ibv.uio.no>
2017-09-06 12:50:01 +0200

    spatial shift in gauss and dog kernels

--- pylgn/kernels/spatial.py
+++ pylgn/kernels/spatial.py
@@ -11,12 +11,12 @@ def create_delta(peak, shift_x=0*pq.deg, shift_y=0*pq.deg):
     Parameters
     ----------
     peak : float
-        Peak value 
+        Peak value
     shift_x : float/quantity scalar
         Shift in x-direction
     shift_y : float/quantity scalar
         Shift in y-direction
-                 
+
     Returns
     -------
     out : function
@@ -28,16 +28,16 @@ def create_delta(peak, shift_x=0*pq.deg, shift_y=0*pq.deg):
 
         Parameters
         ----------
-        x : float/quantity scalar 
-        y : float/quantity scalar    
-           
+        x : float/quantity scalar
+        y : float/quantity scalar
+
         Returns
         -------
         out : ndarray
             Calculated values
         """
         return peak * kronecker_delta(x, shift_x) * kronecker_delta(y, shift_y)
-        
+
     return evaluate
 
 
@@ -51,7 +51,7 @@ def create_delta_ft(shift_x=0*pq.deg, shift_y=0*pq.deg):
         Shift in x-direction
     shift_y : float/quantity scalar
         Shift in y-direction
-                 
+
     Returns
     -------
     out : function
@@ -63,20 +63,21 @@ def create_delta_ft(shift_x=0*pq.deg, shift_y=0*pq.deg):
 
         Parameters
         ----------
-        kx : float/quantity scalar 
-        ky : float/quantity scalar    
-           
+        kx : float/quantity scalar
+        ky : float/quantity scalar
+
         Returns
         -------
         out : ndarray
             Calculated values
         """
         return np.exp(-1j * (kx * shift_x + ky * shift_y))
-        
+
     return evaluate
 
 
-def create_gauss(A=1, a=0.62*pq.deg):
+def create_gauss(A=1, a=0.62*pq.deg,
+                 dx=0.*pq.deg, dy=0.*pq.deg):
     """
     Create Gaussian function closure
 
@@ -86,7 +87,11 @@ def create_gauss(A=1, a=0.62*pq.deg):
         Peak value
     a : float/quantity scalar
         Width
-                 
+    dx: float/quantity scalar
+        shift in x-direction
+    dy: float/quantity scalar
+        shift in y-direction
+
     Returns
     -------
     out : function
@@ -98,23 +103,24 @@ def create_gauss(A=1, a=0.62*pq.deg):
 
         Parameters
         ----------
-        x : float/quantity scalar 
-        y : float/quantity scalar    
-           
+        x : float/quantity scalar
+        y : float/quantity scalar
+
         Returns
         -------
         out : ndarray
             Calculated values
         """
-        r2 = x**2 + y**2
+        r2 = (x-dx)**2 + (y-dy)**2
         return A / a**2 / np.pi * np.exp(-r2/a**2)
-        
+
     return evaluate
 
 
-def create_gauss_ft(A=1, a=0.62*pq.deg):
+def create_gauss_ft(A=1, a=0.62*pq.deg,
+                    dx=0.*pq.deg, dy=0.*pq.deg):
     """
-    Create Fourier transformed 
+    Create Fourier transformed
     Gaussian function closure.
 
     Parameters
@@ -123,7 +129,11 @@ def create_gauss_ft(A=1, a=0.62*pq.deg):
         peak value
     a : float/quantity scalar
         Width
-                 
+    dx: float/quantity scalar
+        shift in x-direction
+    dy: float/quantity scalar
+        shift in y-direction
+
     Returns
     -------
     out : function
@@ -135,23 +145,24 @@ def create_gauss_ft(A=1, a=0.62*pq.deg):
 
         Parameters
         ----------
-        kx : float/quantity scalar 
-        ky : float/quantity scalar    
-           
+        kx : float/quantity scalar
+        ky : float/quantity scalar
+
         Returns
         -------
         out : ndarray
             Calculated values
         """
         k2 = kx**2 + ky**2
-        return A * np.exp(-a**2 * k2/4.)
-        
+        return A * np.exp(-a**2 * k2/4.) * np.exp(-1j * (kx*dx + ky*dy))
+
     return evaluate
 
 
-def create_dog_ft(A=1, a=0.62*pq.deg, B=0.85, b=1.26*pq.deg):
+def create_dog_ft(A=1, a=0.62*pq.deg, B=0.85, b=1.26*pq.deg,
+                  dx=0.*pq.deg, dy=0.*pq.deg):
     """
-    Create Fourier transformed 
+    Create Fourier transformed
     difference of Gaussian function closure
 
     Parameters
@@ -163,37 +174,42 @@ def create_dog_ft(A=1, a=0.62*pq.deg, B=0.85, b=1.26*pq.deg):
     B : float
         Surround peak value
     b : float/quantity scalar
-        Surround width     
-        
+        Surround width
+    dx: float/quantity scalar
+        shift in x-direction
+    dy: float/quantity scalar
+        shift in y-direction
+
     Returns
     -------
     out : function
         Evaluate function
     """
-    center = create_gauss_ft(A, a)
-    surround = create_gauss_ft(B, b)
+    center = create_gauss_ft(A, a, dx, dy)
+    surround = create_gauss_ft(B, b, dx, dy)
 
     def evaluate(kx, ky):
         """
-        Evaluates the Fourier transform of 
+        Evaluates the Fourier transform of
         difference of Gaussian function
 
         Parameters
         ----------
-        kx : float/quantity scalar 
-        ky : float/quantity scalar    
-           
+        kx : float/quantity scalar
+        ky : float/quantity scalar
+
         Returns
         -------
         out : ndarray
             Calculated values
         """
         return center(kx, ky) - surround(kx, ky)
-        
+
     return evaluate
 
 
-def create_dog(A=1, a=0.62*pq.deg, B=0.85, b=1.26*pq.deg):
+def create_dog(A=1, a=0.62*pq.deg, B=0.85, b=1.26*pq.deg,
+               dx=0*pq.deg, dy=0*pq.deg):
     """
     Create difference of Gaussian function closure
 
@@ -206,30 +222,34 @@ def create_dog(A=1, a=0.62*pq.deg, B=0.85, b=1.26*pq.deg):
     B : float
         Surround peak value
     b : float/quantity scalar
-        Surround width    
-         
+        Surround width
+    dx: float/quantity scalar
+        shift in x-direction
+    dy: float/quantity scalar
+        shift in y-direction
+
     Returns
     -------
     out : function
         Evaluate function
     """
-    center = create_gauss(A, a)
-    surround = create_gauss(B, b)
-    
+    center = create_gauss(A, a, dx, dy)
+    surround = create_gauss(B, b, dx, dy)
+
     def evaluate(x, y):
         """
         Evaluates the difference of Gaussian function
 
         Parameters
         ----------
-        x : float/quantity scalar 
-        y : float/quantity scalar    
-           
+        x : float/quantity scalar
+        y : float/quantity scalar
+
         Returns
         -------
         out : ndarray
             Calculated values
         """
         return center(x, y) - surround(x, y)
-        
+
     return evaluate
